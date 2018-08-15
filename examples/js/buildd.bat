03103034bba4da07b2471b6aac13705dcf830170
James Vickers <james@metabench.com>
2018-08-15 03:17:20 +0100

    0.0.41 - Deleted app js bundles - now obselete with build on start

--- examples/js/buildd.bat
+++
@@ -1,6 +0,0 @@
-rem browserify app.js -o app-bundle.js -d --ignore ../cpp/build/Release/fin.node --ignore smalloc
-
-CALL browserify app.js -o app-bundle.js -d --ignore ../cpp/build/Release/fin.node --ignore smalloc
-rem CALL uglifyjs big-app-bundle.js -c hoist_vars=false,if_return=false,keep_fnames -o app-bundle.js
-rem CALL uglifyjs big-app-bundle.js -b -o app-bundle.js
-rem del big-app-bundle.js
--- examples/js/buildr.bat
+++
@@ -1,5 +0,0 @@
-CALL browserify app.js -o big-app-bundle.js --ignore ../cpp/build/Release/fin.node --ignore smalloc
-REM CALL uglifyjs big-app-bundle.js -c hoist_vars=false,if_return=false,keep_fnames -o app-bundle.js
-CALL uglifyjs big-app-bundle.js -c -o app-bundle.js
-del big-app-bundle.js
-REM pause
